212b2b8f89b7ad728cc966ce6fc23523eb4f0e7c
NoRazoom <kakovtoktoto@yandex.ru>
2026-07-01 17:09:44 +0300

    first (pep8) commit

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -45,7 +45,8 @@ async def create_donation(
 @router.get(
     '/my',
     response_model=list[DonationDBCreateNOUser],
-    # response_model_exclude={'user_id'},
+    # использую другую схему (без user_id), посколько не работает
+    #response_model_exclude={'user_id'}
 )
 async def get_my_donations(
     user: Annotated[User, Depends(current_user)],
